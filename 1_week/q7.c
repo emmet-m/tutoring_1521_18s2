888a48c0fcf50af2d9c392e2c89e7ceaec27a1f6
Emmet Murray <e.minor.murray@gmail.com>
2018-07-25 10:00:12 +1000

    Added extra stuff

--- 1_week/q7.c
+++ 1_week/q7.c
@@ -7,7 +7,11 @@ int main (int argc, char * argv[], char * env[]) {
     int n, a, b, c;
     n = scanf("%d %d %d", &a, &b, &c);
 
+    (void) n;
+
     // How can we deal with errors from the above function?
+
+    printf("%s", strerror(errno));
 
     return EXIT_SUCCESS;
 }
--- 1_week/q8.c
+++ 1_week/q8.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int getInt() {
+int getInt(int * err) {
     // ...
     return 0;
 }
