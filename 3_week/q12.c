0a42756ee88f83259166fb52c55ee4e5e4c939e8
Emmet Murray <e.minor.murray@gmail.com>
2018-08-05 15:21:47 +1000

    Updated q12

--- 3_week/q12.c
+++ 3_week/q12.c
@@ -6,11 +6,10 @@ int main(void)
     int x = 100;
     char s[8];
     int y = 200;
-    char z = 0;
 
-    strcpy(s, "a long name asd");
+    strcpy(s, "a long name");
 
-    printf("%s\n", s);
+    printf("%d '%s' %d\n", x, s, y);
     printf("0x%p 0x%p 0x%p\n", &x, s, &y);
 
     return 0;
