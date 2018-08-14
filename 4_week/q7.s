4fb5f07b98f017500d4133cfa6bfac4f1593ff47
Emmet Murray <e.minor.murray@gmail.com>
2018-08-15 09:58:28 +1000

    q7

--- 4_week/q7.s
+++ 4_week/q7.s
@@ -5,6 +5,21 @@ y:  .space 4 # 4 bytes of space, an int
     .globl main
 main:
 
-    # TODO: Everyting
+    li $v0, 5
+    syscall
+    sw $v0, y
+    
+    lw $t0, y
+    # (y + 2000) * (y + 3000)
+    addi $t1, $t0, 2000
+    addi $t2, $t0, 3000
+
+    mult $t1, $t2
+
+    mflo $a0
+
+    li $v0, 1
+    move $a0, $t0
+    syscall
 
     j $ra
