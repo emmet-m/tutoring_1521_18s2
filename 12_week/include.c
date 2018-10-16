a659ae6b6ebd2aa1b192f4399d2c99a15dbd6503
Emmet Murray <e.minor.murray@gmail.com>
2018-10-17 10:40:53 +1100

    Wohoo

---
+++ 12_week/include.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "Students.h"
+
+int main () {
+    return 0;
+}
--- 12_week/q2/Students.c
+++ 12_week/q2/Students.c
@@ -25,7 +25,29 @@ typedef struct _students {
 // build a collection of student records from a file descriptor
 Students getStudents(int in)
 {
-	return NULL;  // TODO ... replace this by your code
+    // INitialise counter and create temporary sturec struct
+    int size = 0;
+    struct _stu_rec tmpStu;
+    // Count amount of sturecs in file specified by 'in'
+    // until there's no more studetns to read.
+    while (read(in, &tmpStu, sizeof(struct _stu_rec)) == sizeof(struct _stu_rec)){
+        size++;
+    }
+
+    // Reset file offset to start
+    lseek(in, 0, SEEK_SET);
+
+    // malloc amount of student records required
+    // inside a students struct
+    struct _students * stus = malloc(sizeof(struct _students));
+    stus->nstu = size;
+    stus->recs = malloc(sizeof(struct _stu_rec)*size);
+
+    // Fill in the sturecs inside the student struct
+    int i = 0;
+    read(in, stus->recs, size*sizeof(struct _stu_rec));
+
+	return stus;  
 }
 
 // show a list of student records pointed to by ss
